12ae27361a5fda458172a244bd5e60b5af1bd0bd
Seeyn <11710503@mail.sustech.edu.cn>
2020-03-22 15:22:40 +0800

    add some features

---
+++ CBIRtool/encoder/Faiss.py
@@ -0,0 +1,35 @@
+import faiss
+import numpy as np
+
+'''
+the input of faiss should be numpy
+'''
+def transform(vectors):
+    #transform list of vectors to numpy
+    return np.array(vectors,dtype=float).astype('float32')
+
+def construct(vectors,Dimension,indexFile,GPU=False):
+    # if GPU:
+    #     res = faiss.StandardGpuResources()
+    #     index = faiss.index_cpu_to_gpu(res, 0, index)
+    #assert type(vectors)==list
+    vectors = transform(vectors)    
+    index=faiss.IndexFlatIP(Dimension)
+    index.add(vectors)
+    with open(indexFile,'w') as file:
+    	file.write('')
+    faiss.write_index(index, indexFile)
+    return  
+
+
+def update(vectors,indexFile):
+    pass
+def search(vector,indexFile,k,GPU = False):
+    vector = transform(vector) 
+    index = faiss.read_index(indexFile)
+    D, I = index.search(vector, k)
+    return D,I
+
+
+
+
---
+++ CBIRtool/transformer/PIL.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def transform(imagePath:str):
+    
+#     transform = transforms.Compose([
+#                 transforms.Resize([256,256])
+# #               transforms.ToTensor()
+# #            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#     ])
+
+    
+    img = Image.open(imagePath)
+    img = img.resize((256,256))
+    #print(img.size,img.mode)
+
+    return img
+
+    
