7817885200264ebf0f6d65e9dcc8c740dc9e1353
Seeyn <11710503@mail.sustech.edu.cn>
2020-03-23 21:59:51 +0800

    Update search.py

--- CBIRtool/search.py
+++ CBIRtool/search.py
@@ -3,26 +3,24 @@ from extractor import Resnet
 from encoder import Faiss
 import psycopg2
 
-# def search(imagePath,indexFile):
+def search(imagePath,indexFile):
     
-#     e = Resnet.resnet('cpu')
-#     t = PIL.transform(imagePath)
-#     Faiss.search(e.extract(t),indexFile,2)
+    e = Resnet.resnet('cpu')
+    t = PIL.transform(imagePath)
+    distance,index = Faiss.search(e.extract(t),indexFile,2)
+    conn = psycopg2.connect(database="test", user="lee", password="666666", host="127.0.0.1", port="5432") 
+    cur = conn.cursor()
+
+    sql = 'select img from imgTable'
+    cur.execute(sql)
+    rows = cur.fetchall()
+
+    print(type(rows))
+    i = 0
+    for row in rows:
+        with open('test{}.jpeg'.format(i),'wb') as file:
+            file.write(row[0])
+        i+=1
 
 
-conn = psycopg2.connect(database="test", user="lee", password="666666", host="127.0.0.1", port="5432") 
-cur = conn.cursor()
-
-sql = 'select img from imgTable'
-cur.execute(sql)
-rows = cur.fetchall()
-
-print(type(rows))
-i = 0
-for row in rows:
-    with open('test{}.jpeg'.format(i),'wb') as file:
-    	file.write(row[0])
-    i+=1
-
-
-conn.commit()
+    conn.commit()
