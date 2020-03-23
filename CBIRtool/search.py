9039b6494d3f68ee480a0b784c4f8049f4958c43
Seeyn <11710503@mail.sustech.edu.cn>
2020-03-23 16:54:05 +0800

    test search

--- CBIRtool/search.py
+++ CBIRtool/search.py
@@ -1,10 +1,24 @@
 from transformer import PIL
 from extractor import Resnet
 from encoder import Faiss
+import psycopg2
 
-
-def search(imagePath,indexFile):
+# def search(imagePath,indexFile):
     
-    e = Resnet.resnet('cpu')
-    t = PIL.transform(imagePath)
-    Faiss.search(e.extract(t),indexFile,5)
+#     e = Resnet.resnet('cpu')
+#     t = PIL.transform(imagePath)
+#     Faiss.search(e.extract(t),indexFile,2)
+
+
+conn = psycopg2.connect(database="test", user="lee", password="666666", host="127.0.0.1", port="5432") 
+cur = conn.cursor()
+
+sql = 'select img from imgTable limit 1 offset 1'
+cur.execute(sql)
+rows = cur.fetchall()
+for row in rows:
+    with open('test.jpeg','wb') as file:
+    	file.write(row)
+
+
+conn.commit()
