2208c9e6d213529ce363314d18b43398b2c42c92
Caiyongning <andyb1ance@localhost.localdomain>
2020-03-24 15:14:41 +0800

    search_success

--- myproject/myproject/hello.py
+++ myproject/myproject/hello.py
@@ -71,4 +71,4 @@ def hello():
     return render_template('Search.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(debug=False,host='0.0.0.0', port=5000)
