9dbb0921aa7beed5983c51e6dcb533923cb7f1f9
Caiyongning <andyb1ance@localhost.localdomain>
2020-04-12 21:48:37 +0800

    complete_test

--- myproject/myproject/test.py
+++ myproject/myproject/test.py
@@ -2,7 +2,10 @@ import CBIRtool
 import os
 
 a = CBIRtool.frame.Framework("Resnet34","Faiss",'./CBIRtool/encoder/index/sample.index')
-os.chdir('./sample')
-a.construct(os.listdir())
+folder = './sample'
+filelist = os.listdir(folder)
+for i,v in enumerate(filelist):
+    filelist[i]=os.path.join(folder,v)
+a.construct(filelist)
 
 print(a.search('./sample/10_left.jpeg',3))
